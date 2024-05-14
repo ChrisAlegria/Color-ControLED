522ce454b185abe35981a849a3ff8ab6d7e446d9
ChrisAlegria <christian.alegriar.com>
2024-05-14 00:59:20 -0600

    Init: Color Picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,138 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ColorPickerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ColorPickerPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ColorPickerPageState createState() => _ColorPickerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _ColorPickerPageState extends State<ColorPickerPage> {
+  Color selectedColor = Colors.white;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Color Picker'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ColorWheel(
+          onColorSelected: (color) {
+            setState(() {
+              selectedColor = color;
+            });
+          },
+          selectedColor: selectedColor,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class ColorWheel extends StatelessWidget {
+  final Function(Color) onColorSelected;
+  final Color selectedColor;
+
+  ColorWheel({required this.onColorSelected, required this.selectedColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double wheelSize = constraints.maxWidth * 0.8;
+        double selectedCircleSize = wheelSize * 0.5;
+
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: wheelSize,
+              height: wheelSize,
+              child: GestureDetector(
+                onPanUpdate: (details) {
+                  RenderBox renderBox = context.findRenderObject() as RenderBox;
+                  var localPosition =
+                      renderBox.globalToLocal(details.globalPosition);
+                  var dx = localPosition.dx - wheelSize / 2;
+                  var dy = localPosition.dy - wheelSize / 2;
+                  var angle = atan2(dy, dx);
+                  var hue = angle < 0 ? angle / (2 * pi) + 1 : angle / (2 * pi);
+                  var color =
+                      HSVColor.fromAHSV(1.0, hue * 360, 1.0, 1.0).toColor();
+                  onColorSelected(color);
+                },
+                child: CustomPaint(
+                  painter: ColorWheelPainter(selectedColor: selectedColor),
+                ),
+              ),
+            ),
+            Container(
+              width: selectedCircleSize,
+              height: selectedCircleSize,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: selectedColor,
+                border: Border.all(color: Colors.black),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class ColorWheelPainter extends CustomPainter {
+  final Color selectedColor;
+
+  ColorWheelPainter({required this.selectedColor});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double strokeWidth = 4.0;
+    final double radius = size.width / 2;
+    final double centerX = size.width / 2;
+    final double centerY = size.height / 2;
+
+    for (int i = 0; i < 360; i++) {
+      final double hue = i.toDouble();
+      final Paint paint = Paint()
+        ..color = HSVColor.fromAHSV(1.0, hue, 1.0, 1.0).toColor()
+        ..strokeWidth = strokeWidth
+        ..strokeCap = StrokeCap.round;
+
+      final double x1 =
+          centerX + (radius - strokeWidth / 2) * cos(i * pi / 180);
+      final double y1 =
+          centerY + (radius - strokeWidth / 2) * sin(i * pi / 180);
+      final double x2 =
+          centerX + (radius + strokeWidth / 2) * cos(i * pi / 180);
+      final double y2 =
+          centerY + (radius + strokeWidth / 2) * sin(i * pi / 180);
+
+      canvas.drawLine(
+        Offset(x1, y1),
+        Offset(x2, y2),
+        paint,
+      );
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
